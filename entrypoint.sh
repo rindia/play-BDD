4dd427a792bc67fd7e8d0705d8d95aa6e2f27b72
Rohit Saini <rohit.saini.uk@gmail.com>
2024-01-23 13:04:02 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # Default values
-ENV_VAR=""
-TAGS_VAR=""
+ENV=""
+TAGS=""
 
-if [ -n "$ENV" ]; then
-  ENV_VAR="$ENV"
-fi
-
-if [ -n "$TAGS" ]; then
-  TAGS_VAR="$TAGS"
-fi
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --env)
+            ENV=$2
+            shift 2
+            ;;
+        --tags)
+            TAGS=$2
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
 echo "+++++++++++++++"
 echo npm run test:reportPortal --ENV="$ENV_VAR" --TAGS="$TAGS_VAR"
 npm run test:reportPortal --ENV="$ENV_VAR" --TAGS="$TAGS_VAR"
