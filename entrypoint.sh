227f2f98ee186c766d6aef505c7362f9d7e07c18
Rohit Saini <rohit.saini.uk@gmail.com>
2024-01-23 13:13:23 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# Default values
-ENV=""
-TAGS=""
+# Use the environment variables in your script
+echo "Environment: $ENV"
+echo "Tags: $TAGS"
 
-while [ "$#" -gt 0 ]; do
-    case "$1" in
-        --env)
-            ENV=$2
-            shift 2
-            ;;
-        --tags)
-            TAGS=$2
-            shift 2
-            ;;
-        *)
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-    esac
-done
-echo "+++++++++++++++"
-echo npm run test:reportPortal --ENV="$ENV_VAR" --TAGS="$TAGS_VAR"
-npm run test:reportPortal --ENV="$ENV_VAR" --TAGS="$TAGS_VAR"
+# Add the rest of your entrypoint script logic here
+# ...
