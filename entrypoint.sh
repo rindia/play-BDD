b43ad9fa111b06fc96dc8953a54e38f39fe956b8
Rohit Saini <rohit.saini.uk@gmail.com>
2024-01-23 14:12:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Default values
-ENV_VAR="apiQA"
-TAGS_VAR="@api"
+ENV_VAR=""
+TAGS_VAR=""
+BROWSER_VAR=""
+PARALLEL_VAR=""
 
 if [ -n "$ENV" ]; then
   ENV_VAR="$ENV"
@@ -11,6 +13,15 @@ fi
 if [ -n "$TAGS" ]; then
   TAGS_VAR="$TAGS"
 fi
+
+if [ -n "$BROWSER" ]; then
+  BROWSER_VAR="$BROWSER"
+fi
+
+if [ -n "$PARALLE" ]; then
+  PARALLEL_VAR="$PARALLE"
+fi
+
 echo npx cross-env npm_config_ENV=$ENV_VAR  npm_config_TAGS=$TAGS_VAR npm run test:reportPortal
-npx cross-env npm_config_ENV=$ENV_VAR  npm_config_TAGS=$TAGS_VAR npm run test:reportPortal
+npx cross-env npm_config_ENV=$ENV_VAR  npm_config_TAGS=$TAGS_VAR npm_config_BROWSER=$BROWSER_VAR npm_config_PARALLE=$PARALLE_VAR npm run test:reportPortal
 
