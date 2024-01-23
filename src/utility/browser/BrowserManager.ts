49836fa9f9e9e8a0dfe46a18e1032882d06b51f0
Rohit Saini <rohit.saini.uk@gmail.com>
2024-01-23 11:53:59 +0530

    Update BrowserManager.ts

--- src/utility/browser/BrowserManager.ts
+++ src/utility/browser/BrowserManager.ts
@@ -1,7 +1,7 @@
 import { LaunchOptions, chromium, firefox, webkit } from "@playwright/test";
 
 const options: LaunchOptions = {
-    headless: false,
+    headless: headless: process.env.npm_config_HEADLESS === "true" || true,
     timeout: 30000
 }
 
