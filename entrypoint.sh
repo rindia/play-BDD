c4611610e6c7fd6a70ddec2e80f68b215bcc3fce
Rohit Saini <rohit.saini.uk@gmail.com>
2024-01-23 14:27:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ ENV_VAR=""
 TAGS_VAR=""
 BROWSER_VAR=""
 PARALLEL_VAR=""
+LAUNCH_VAR=""
 
 if [ -n "$ENV" ]; then
   ENV_VAR="$ENV"
@@ -22,8 +23,12 @@ if [ -n "$PARALLEL" ]; then
   PARALLEL_VAR="$PARALLEL"
 fi
 
+if [ -n "$LAUNCH" ]; then
+ LAUNCH_VAR="$LAUNCH"
+fi
+
 echo "enviornment values"
 echo npm_config_ENV=$ENV_VAR  npm_config_TAGS=$TAGS_VAR npm_config_BROWSER=$BROWSER_VAR npm_config_PARALLEL=$PARALLEL_VAR npm_config_HEADLESS="true"
 
-npx cross-env npm_config_ENV=$ENV_VAR  npm_config_TAGS=$TAGS_VAR npm_config_BROWSER=$BROWSER_VAR npm_config_PARALLEL=$PARALLEL_VAR npm_config_HEADLESS="true" npm run test:reportPortal
+npx cross-env npm_config_ENV=$ENV_VAR  npm_config_TAGS=$TAGS_VAR npm_config_BROWSER=$BROWSER_VAR npm_config_PARALLEL=$PARALLEL_VAR npm_config_HEADLESS="true" npm_config_LAUNCH=$LAUNCH_VAR npm run test:reportPortal
 
