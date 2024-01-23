ac9b9c74a483a7d2e2a4a341a8b4fc888971332a
Rohit Saini <rohit.saini.uk@gmail.com>
2024-01-23 14:00:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,5 @@ if [ -n "$TAGS" ]; then
   TAGS_VAR="$TAGS"
 fi
 echo npm run test:reportPortal ENV="$ENV_VAR" TAGS="$TAGS_VAR"
-npx cross-env npm_config_ENV=webQA  npm_config_TAGS=@test npm run test:local
+npx cross-env npm_config_ENV=$ENV_VAR  npm_config_TAGS=$TAGS_VAR npm run test:reportPortal
 
